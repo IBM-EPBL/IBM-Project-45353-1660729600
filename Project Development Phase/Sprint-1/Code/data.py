9bcb70fcdab388ea24c6d77af7f0e9ea9d2525fa
Thirumavalavan <riothiru2002@outlook.com>
2022-11-18 15:18:54 +0530

    Added Code Files in Sprint Folders

---
+++ Project Development Phase/Sprint-1/Code/data.py
@@ -0,0 +1,110 @@
+job = {
+    'programming':[
+        {
+        'title':'Front-End Developer',
+        'salary': '₹3L per year',
+        'skills':['HTML', 'CSS', 'Javascript', 'React', 'Angular'],
+        'link': 'https://www.naukri.com/front-end-developer-jobs'
+        },
+        {
+        'title': 'Full Stack Developer',
+        'salary': '₹6.5L per year',
+        'skills':['Frontend Languages', 'Backend Technologies', 'DBMS'],
+        'link': 'https://www.naukri.com/full-stack-developer-jobs'
+        },
+        {
+        'title': 'Android Developer',
+        'salary': '₹5.5L per year',
+        'skills':['SQL', 'Java', 'Javascript', 'C++', 'PHP', 'Python'],
+        'link': 'https://www.naukri.com/full-stack-developer-jobs'
+        },
+        {
+        'title': 'Big Data Engineer',
+        'salary': '₹7.8L per year',
+        'skills':['Scripting Languages', 'Java', 'Python', 'MongoDB', 'redis'],
+        'link': 'https://www.naukri.com/full-stack-developer-jobs'
+        },
+        {
+        'title': 'AWS Developer',
+        'salary': '₹6.5L per year',
+        'skills':['Cloud Based Monitoring', 'Cloud Security tools', 'DBMS'],
+        'link': 'https://www.naukri.com/full-stack-developer-jobs'
+        },
+        {
+        'title': 'Robotics Engineer',
+        'salary': '₹5L per year',
+        'skills':['C', 'C++', 'Python', 'Java'],
+        'link': 'https://www.naukri.com/full-stack-developer-jobs'
+        },
+        {
+        'title': 'Software Engineer',
+        'salary': '₹6.5L per year',
+        'skills':['Java', 'Python', '.Net', 'Mean', 'Ruby'],
+        'link': 'https://www.naukri.com/full-stack-developer-jobs'
+        },
+        {
+        'title': 'Blockchain Developer',
+        'salary': '₹7.5L per year',
+        'skills':['Java', 'Python', 'C++', 'Data Structure', 'Backend Development','Cryptography'],
+        'link': 'https://www.naukri.com/full-stack-developer-jobs'
+        },
+        {
+        'title': 'Machine Learning Engineer',
+        'salary': '₹9L per year',
+        'skills':['Python', 'C++', 'Data modelling', 'A.I'],
+        'link': 'https://www.naukri.com/full-stack-developer-jobs'
+        },
+        {
+        'title': 'Data Scientists',
+        'salary': '₹11L per year',
+        'skills':['Machine learning', 'Deep learning', 'Data Visualization', 'Data Wrangling'],
+        'link': 'https://www.naukri.com/full-stack-developer-jobs'
+        },
+    ],
+    'non-programming': [
+        {
+        'title': 'Business Analyst',
+        'salary': '₹6L per year',
+        'skills':['MS Office', 'Interpersonal skills', 'Consultative skills'],
+        'link': 'https://www.naukri.com/business-analyst-jobs'
+        },
+        {
+        'title': 'UI/UX Designer',
+        'salary': '₹5.1L per year',
+        'skills':['Prototyping', ' wireframing', 'mockups', 'user flows','Industry knowledge', 'Focus on a specialty'],
+        'link': 'https://www.naukri.com/business-analyst-jobs'
+        },
+        {
+        'title': 'System Admin',
+        'salary': '₹6.9L per year',
+        'skills':['Technical Skills', 'Operating System', 'Organizational Skills','Attention to Detail','Networking'],
+        'link': 'https://www.naukri.com/business-analyst-jobs'
+        },
+        {
+        'title': 'Technical Writer',
+        'salary': '₹6.5L per year',
+        'skills':['Clear understanding', 'User persona targeting', 'Technical documentation essentials', 'Basic graphic', ' web design', 'Research skills'],
+        'link': 'https://www.naukri.com/business-analyst-jobs'
+        },
+        {
+        'title': 'QA Manual Testing',
+        'salary': '₹6.3L per year',
+        'skills':['Attention to detail', 'Strong organizational skills'],
+        'link': 'https://www.naukri.com/business-analyst-jobs'
+        },
+        {
+        'title': 'Human Resource',
+        'salary': '₹6L per year',
+        'skills':['Employee relations', 'Project management', 'Advising', 'Coaching'],
+        'link': 'https://www.naukri.com/business-analyst-jobs'
+        },
+        {
+        'title': 'Devops Engineer',
+        'salary': '₹7.5L per year',
+        'skills':['Saas', 'Plateform tools(Chef, Puppet, Docker)'],
+        'link': 'https://www.naukri.com/business-analyst-jobs'
+        },
+    ],
+}
+ 
+
